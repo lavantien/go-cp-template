5ff1cf756fa0b5a6c69801ad2df860b61647bfe0
lavantien <lavantien96@gmail.com>
2021-12-20 02:31:13 +0700

    AoC 2021: Day 1 - Part 1

--- main.go
+++ main.go
@@ -1,27 +1,48 @@
 package main
 
-import "fmt"
+import (
+	_ "embed"
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	var tc int
-	fmt.Scan(&tc)
-	for t := 0; t < tc; t++ {
-		var n int
+	inType := "file"
+	var arr []int
+	var n int
+	switch inType {
+	case "file":
+		strs := strings.Fields(inStr)
+		n = len(strs)
+		arr = make([]int, n)
+		var err error
+		for i := 0; i < n; i++ {
+			arr[i], err = strconv.Atoi(strs[i])
+			if err != nil {
+				continue
+			}
+		}
+	case "std":
 		fmt.Scan(&n)
-		arr := make([]int, n)
 		for i := 0; i < n; i++ {
 			var t int
 			fmt.Scan(&t)
-			arr[i] = t
+			arr = append(arr, t)
 		}
-		rs := solution(arr, n)
-		fmt.Println(rs)
 	}
+	rs := solution(arr, n)
+	fmt.Println(rs)
 }
 
 func solution(arr []int, n int) (rs int) {
-	for i := 0; i < n; i++ {
-		rs += arr[i]
+	for i := 1; i < n; i++ {
+		if arr[i] > arr[i-1] {
+			rs++
+		}
 	}
 	return
 }
+
+//go:embed input.txt
+var inStr string
