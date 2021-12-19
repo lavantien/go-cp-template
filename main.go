fae7f27530e2d92794777bf4e642195f772e6c39
lavantien <lavantien96@gmail.com>
2021-12-20 05:00:24 +0700

    AoC 2021: Day 3 - Part 1

--- main.go
+++ main.go
@@ -16,24 +16,40 @@ func main() {
 		str = inStr
 	}
 	scn := bufio.NewScanner(strings.NewReader(str))
-	distance := 0
-	depth := 0
-	aim := 0
-	for scn.Scan() {
-		var cmd string
-		var val int
-		fmt.Sscanln(scn.Text(), &cmd, &val)
-		switch cmd {
-		case "forward":
-			distance += val
-			depth += aim * val
-		case "up":
-			aim -= val
-		case "down":
-			aim += val
+	var wordSize int
+	n := 0
+	n++
+	scn.Scan()
+	tstr := scn.Text()
+	wordSize = len(tstr)
+	count := make([]int, wordSize)
+	for i := 0; i < wordSize; i++ {
+		if tstr[i] == '1' {
+			count[i]++
 		}
 	}
-	rs := distance * depth
+	for scn.Scan() {
+		n++
+		tstr := scn.Text()
+		for i := 0; i < wordSize; i++ {
+			if tstr[i] == '1' {
+				count[i]++
+			}
+		}
+	}
+	gamma := make([]int, wordSize)
+	epsilon := make([]int, wordSize)
+	halfSize := n / 2
+	for i := 0; i < wordSize; i++ {
+		if count[i] > halfSize {
+			gamma[i] = 1
+			epsilon[i] = 0
+		} else {
+			gamma[i] = 0
+			epsilon[i] = 1
+		}
+	}
+	rs := wordToInt(gamma[:]) * wordToInt(epsilon[:])
 	fmt.Println(rs)
 }
 
@@ -42,3 +58,19 @@ var inStr string
 
 //go:embed test.txt
 var testStr string
+
+func wordToInt(word []int) (rs int) {
+	n := len(word)
+	for i := n - 1; i >= 0; i-- {
+		rs += word[i] * pw2(n-i-1)
+	}
+	return
+}
+
+func pw2(x int) (rs int) {
+	rs = 1
+	for i := 0; i < x; i++ {
+		rs *= 2
+	}
+	return
+}
