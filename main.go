5500d81f2583e0ae21b8d34c67e672b3f6941603
lavantien <lavantien96@gmail.com>
2021-12-20 01:54:56 +0700

    sum of array

--- main.go
+++ main.go
@@ -3,5 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("it works")
+	var tc int
+	fmt.Scan(&tc)
+	for t := 0; t < tc; t++ {
+		var n int
+		fmt.Scan(&n)
+		arr := make([]int, n)
+		for i := 0; i < n; i++ {
+			var t int
+			fmt.Scan(&t)
+			arr[i] = t
+		}
+		rs := solution(arr, n)
+		fmt.Println(rs)
+	}
+}
+
+func solution(arr []int, n int) (rs int) {
+	for i := 0; i < n; i++ {
+		rs += arr[i]
+	}
+	return
 }
