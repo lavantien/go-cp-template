28b6b6c2e641941cbd377c6cee86643240e68866
lavantien <lavantien96@gmail.com>
2021-12-20 03:19:39 +0700

    AoC 2021: Day 2 - Part 1

---
+++ README.md
@@ -0,0 +1,5 @@
+# Go Competitive Programming Template
+
+## How to use this repo?
+
+Check the commit history, each commit is either for solving an Advent of Code problem, or a Competitive Programming problem elsewhere
--- main.go
+++ main.go
@@ -1,51 +1,42 @@
 package main
 
 import (
+	"bufio"
 	_ "embed"
 	"fmt"
-	"strconv"
 	"strings"
 )
 
 func main() {
-	inType := "file"
-	var arr []int
-	var n int
-	switch inType {
-	case "file":
-		strs := strings.Fields(inStr)
-		n = len(strs)
-		arr = make([]int, n)
-		var err error
-		for i := 0; i < n; i++ {
-			arr[i], err = strconv.Atoi(strs[i])
-			if err != nil {
-				continue
-			}
-		}
-	case "std":
-		fmt.Scan(&n)
-		for i := 0; i < n; i++ {
-			var t int
-			fmt.Scan(&t)
-			arr = append(arr, t)
+	test := 0
+	var str string
+	if test == 1 {
+		str = testStr
+	} else {
+		str = inStr
+	}
+	scn := bufio.NewScanner(strings.NewReader(str))
+	distance := 0
+	depth := 0
+	for scn.Scan() {
+		var cmd string
+		var val int
+		fmt.Sscanln(scn.Text(), &cmd, &val)
+		switch cmd {
+		case "forward":
+			distance += val
+		case "up":
+			depth -= val
+		case "down":
+			depth += val
 		}
 	}
-	rs := solution(arr, n)
+	rs := distance * depth
 	fmt.Println(rs)
-}
-
-func solution(arr []int, n int) (rs int) {
-	prevSum := arr[0] + arr[1] + arr[2]
-	for i := 3; i < n; i++ {
-		currSum := arr[i-2] + arr[i-1] + arr[i]
-		if currSum > prevSum {
-			rs++
-		}
-		prevSum = currSum
-	}
-	return
 }
 
 //go:embed input.txt
 var inStr string
+
+//go:embed test.txt
+var testStr string
